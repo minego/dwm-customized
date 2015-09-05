430066cddfb30d2997e7ed04070fbb1b861e3ee4
Micah N Gorrell <micahg@netmail.com>
2015-09-05 16:04:04 -0600

    Applied the combo patch

--- config.h
+++ config.h
@@ -63,9 +63,10 @@ static const Layout layouts[] = {		/* First entry is default */
 
 /* key definitions */
 #define MODKEY Mod1Mask
-// TODO Take my old TAGKEYS from original version
 #define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      comboview,      {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      combotag,       {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
