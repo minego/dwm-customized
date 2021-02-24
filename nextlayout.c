1ed91c2aac917164c647be2ab4971de537a315f6
Micah N Gorrell <micah.gorrell@venafi.com>
2021-02-24 09:51:23 -0700

    Applied the nextlayout patch

---
+++ nextlayout.c
@@ -0,0 +1,15 @@
+static void nextlayout(const Arg *arg) {
+
+    Layout *l;
+
+	for (l = (Layout *)layouts; l != selmon->lt[selmon->sellt]; l++) {
+		;
+	}
+
+    if (l->symbol && (l + 1)->symbol) {
+        setlayout(&((Arg) { .v = (l + 1) }));
+    } else {
+        setlayout(&((Arg) { .v = layouts }));
+	}
+}
+
